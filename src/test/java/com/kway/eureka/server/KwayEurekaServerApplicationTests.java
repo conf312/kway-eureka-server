584e75124727fd8adbace9d4835aac1152cec78b
venh <conf312@naver.com>
2022-09-13 18:20:19 +0900

    initialize kway eureka server

---
+++ src/test/java/com/kway/eureka/server/KwayEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kway.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwayEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
